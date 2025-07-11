85e7b846818d19b0b1703199e72970d07414d676
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-07-12 00:06:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -1,25 +1,26 @@
 class Solution {
   public:
-    int fun(int n, int sum,int sum1,int sum2,vector<int>& arr,int d,vector<vector<int>>&dp){
+    int fun(int n,int target, vector<int>& arr,vector<vector<int>>&dp){
         if(n==0){
-            // try both choices for last element
-            int cnt=0;
-            if(sum1 - sum2 == d) cnt++;
-            if((sum1+arr[0]) - (sum2 - arr[0]) == d) cnt++;
-            return cnt;
+            if(target==0 && arr[0]==0) return 2;
+            if(target ==0 || target==arr[0]) return 1;
+            return 0;
         }
-        if(dp[n][sum1]!=-1) return dp[n][sum1];
-        int nottake=fun(n-1,sum,sum1,sum2,arr,d,dp);
+        if(dp[n][target]!=-1) return dp[n][target];
+        int nottake=fun(n-1,target,arr,dp);
         int take=0;
-        take=fun(n-1,sum,sum1+arr[n],sum2-arr[n],arr,d,dp);
-        return dp[n][sum1] = nottake+take;
+        if(target>=arr[n]) take=fun(n-1,target-arr[n],arr,dp);
+        return dp[n][target] =take+nottake;
     }
     int countPartitions(vector<int>& arr, int d) {
         // Code here
         int n=arr.size();
         int sum=0;
-        for(int i=0;i<n;i++) sum+=arr[i];
-        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-        return fun(n-1,sum,0,sum,arr,d,dp);
+        for(auto i:arr) sum+=i;
+        int target=(d+sum)/2;
+        if((d+sum)%2!=0) return 0;
+        vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        return fun(n-1,target,arr,dp);
+        
     }
 };
