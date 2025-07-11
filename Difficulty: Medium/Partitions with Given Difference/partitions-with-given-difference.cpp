79ea0e1c36edff15327c9f6b1271763fd56c4d78
Mahendra Bansode <153123561+mahendra8432@users.noreply.github.com>
2025-07-12 00:55:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -1,17 +1,7 @@
 class Solution {
   public:
-    int fun(int n,int target, vector<int>& arr,vector<vector<int>>&dp){
-        if(n==0){
-            if(target==0 && arr[0]==0) return 2;
-            if(target ==0 || target==arr[0]) return 1;
-            return 0;
-        }
-        if(dp[n][target]!=-1) return dp[n][target];
-        int nottake=fun(n-1,target,arr,dp);
-        int take=0;
-        if(target>=arr[n]) take=fun(n-1,target-arr[n],arr,dp);
-        return dp[n][target] =take+nottake;
-    }
+  // Method 2:-Tabulation.
+   int mod=1e8;
     int countPartitions(vector<int>& arr, int d) {
         // Code here
         int n=arr.size();
@@ -19,8 +9,20 @@ class Solution {
         for(auto i:arr) sum+=i;
         int target=(d+sum)/2;
         if((d+sum)%2!=0) return 0;
-        vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        return fun(n-1,target,arr,dp);
-        
+        vector<vector<int>>dp(n,vector<int>(target+1,0));
+        int take=0,nottake=0;
+         if (arr[0] == 0) dp[0][0] = 2;  // include or exclude 0
+         else dp[0][0] = 1;  // only exclude
+        // Base case: sum = arr[0]
+        if (arr[0] != 0 && arr[0] <= target) dp[0][arr[0]] = 1;
+        for(int i=1;i<n;i++){
+            for(int tar=0;tar<=target;tar++){
+                nottake=0,take=0;
+                nottake= dp[i-1][tar];
+                if(tar>=arr[i]) take=  dp[i-1][tar-arr[i]];
+                dp[i][tar]= (nottake + take);
+            }
+        }
+        return (dp[n-1][target])% mod;
     }
 };
